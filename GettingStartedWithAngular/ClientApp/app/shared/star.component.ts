260a1bdbf164cdfce522fc35e652795a65db81de
Ousmane Barry <obarrypublic@gmail.com>
2018-05-01 23:49:52 +0200

    Raising event from nested component to container using @Output and Event Binding

--- GettingStartedWithAngular/ClientApp/app/shared/star.component.ts
+++ GettingStartedWithAngular/ClientApp/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnChanges, SimpleChanges, Input } from "@angular/core"
+﻿import { Component, OnChanges, SimpleChanges, Input, Output, EventEmitter } from "@angular/core"
 
 @Component({
     selector: 'star',
@@ -8,8 +8,14 @@
 export class StarComponent implements OnChanges {
     startWidth: number;
     @Input() rating: number;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
     ngOnChanges(changes: SimpleChanges): void {
          this.startWidth = this.rating * 86/5;
     }
+
+    onClick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked`);
+    }
+
 }
