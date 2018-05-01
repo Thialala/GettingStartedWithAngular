2edba54f11a1192659d1d2677c78ca1cddecca57
Ousmane Barry <obarrypublic@gmail.com>
2018-05-01 16:13:37 +0200

    Added interface for Product

--- GettingStartedWithAngular/ClientApp/app/components/products/product-list.component.ts
+++ GettingStartedWithAngular/ClientApp/app/components/products/product-list.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component } from "@angular/core"
+import { IProduct } from './product';
 
 @Component({
     selector: 'products',
@@ -10,7 +11,7 @@ export class ProductListComponent {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
