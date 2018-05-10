cb4bc1af8b4e9d754b5bfba0dd63d2aae522e238
Ousmane Barry <obarrypublic@gmail.com>
2018-05-10 23:21:25 +0200

    Restructure the folders to adhere to Angular

--- GettingStartedWithAngular/ClientApp/app/app.shared.module.ts
+++ GettingStartedWithAngular/ClientApp/app/app.shared.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from "@angular/forms";
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './components/app/app.component';
-import { ProductListComponent } from './components/products/product-list.component';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from "./shared/ConvertToSpacesPipe";
 import { StarComponent } from "./shared/star.component";
-import { HomeComponent } from "./components/home/home.component";
-import { ProductDetailComponent } from "./components/products/product-detail.component";
+import { HomeComponent } from "./home/home.component";
+import { ProductDetailComponent } from "./products/product-detail.component";
+import { ProductGuardService } from './products/product-guardservice';
 
 
 @NgModule({
@@ -28,11 +29,12 @@ import { ProductDetailComponent } from "./components/products/product-detail.com
         RouterModule.forRoot([
             { path: 'home', component: HomeComponent },
             { path: 'products', component: ProductListComponent },
-            { path: 'products/:id', component: ProductDetailComponent },
+            { path: 'products/:id', canActivate: [ProductGuardService], component: ProductDetailComponent },
             { path: '', component: HomeComponent, pathMatch: 'full' },
             { path: '**', component: HomeComponent, pathMatch: 'full' }
         ])
-    ]
+    ],
+    providers: [ProductGuardService]
 })
 export class AppModuleShared {
 }
