0e8a22bcaf09d6ed0399bf3a2df7bf7b886a93bb
Ousmane Barry <obarrypublic@gmail.com>
2018-05-13 12:19:22 +0200

    Put app routing to its own module

--- GettingStartedWithAngular/ClientApp/app/app.shared.module.ts
+++ GettingStartedWithAngular/ClientApp/app/app.shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ProductModule } from "./products/product.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./home/home.component";
@@ -20,11 +20,7 @@ import { HomeComponent } from "./home/home.component";
         FormsModule,
         HttpClientModule,
         ProductModule,
-        RouterModule.forRoot([
-            { path: 'home', component: HomeComponent },
-            { path: '', component: HomeComponent, pathMatch: 'full' },
-            { path: '**', component: HomeComponent, pathMatch: 'full' }
-        ])
+        AppRoutingModule
     ],
 })
 export class AppModuleShared {
